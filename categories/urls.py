59bdb37cf4ae56698a7c2be0f10f78d396d6b2eb
rei <rei@localhost.localdomain>
2023-11-24 22:31:11 -0500

    Version 3

--- categories/urls.py
+++ categories/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from .views import (
     categories_view, 
+    products_category_view,
+    
     create_category_view, 
     update_category_view,
     delete_category_view,
@@ -11,6 +13,8 @@ app_name = 'categories'
 
 urlpatterns = [
     path('', categories_view, name='categories'),
+    path('products_category/<int:pk>', products_category_view, name="products_category"),
+    
     path('create_category/', create_category_view, name='create_category'),
     path('update_category/<int:pk>', update_category_view, name='update_category'),
     path('delete_category/<int:pk>', delete_category_view, name='delete_category'),
--- categories/views.py
+++ categories/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
 from .models import CategoryModel
+from products.models import ProductModel
 from .forms import CategoryForm
 
 # Create your views here.
@@ -12,6 +13,14 @@ def categories_view(request):
     categories = CategoryModel.objects.filter(user=request.user)
     return render(request, 'categories/categories.html', {
         'categories' : categories,
+    })
+
+def products_category_view(request, pk):
+    category = get_object_or_404(CategoryModel, pk=pk, user=request.user)
+    products_category = ProductModel.objects.filter(user=request.user, category=pk)
+    return render(request, 'categories/products_category.html', {
+        'category' : category,
+        'products' : products_category,
     })
 
 @login_required
